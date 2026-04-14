1da4c507037d93cd68463e88c4421cd818472126
GordonSoftbyte <gordon.gichini@softbyte.co.ke>
2026-04-14 09:58:37 +0300

    Smartphone with apps

--- backend.py
+++ backend.py
@@ -4,6 +4,9 @@ class Smartphone:
         self.battery = 100
         self.battery_saver_mode = False 
 
+        self.photos_app = PhotosApp()
+        self.pictagram_app = PictagramApp()
+
     def use_battery(self, amount):
         self.battery -= amount
         if self.battery < 0:
@@ -15,13 +18,56 @@ class Smartphone:
         else:
             self.battery = 100
 
+    def get_total_storage_used(self):
+        return (
+            self.photos_app.calculate_storage_used() + 
+            self.pictagram_app.calculate_storage_used()
+        )
+    
+    def get_storage_left(self):
+        return self.storage_capacity - self.get_total_storage_used()
+    
+    def use_app_battery(self):
+        if self.battery < 2:
+            raise Exception("Battery too low, Charge your Phone!")
+        
+        self.use_battery(2)
+
+    def take_photo(self):
+        if self.get_storage_left() < (24 / 1024):
+            raise Exception("Not enough storage to take a photo!")
+        
+        self.use_app_battery()
+        self.photos_app.take_photo()
+
+    def delete_photo(self):
+        self.use_app_battery()
+        self.photos_app.delete_photo()
+
+    def create_post(self, caption):
+        if self.get_storage_left() < (15 / 1024):
+            raise Exception("Not enough storage to create a post!")
+        
+        self.use_app_battery()
+        self.pictagram_app.create_post(caption)
+
+    def delete_post(self):
+        self.use_app_battery()
+        self.pictagram_app.delete_post()
+
+
+
     def __str__(self):
         if self.battery_saver_mode:
             mode = "Enabled"
         else:
             mode = "Disabled"
 
-        return f"BnL Smartphone - Storage: {self.storage_capacity}GB, Battery: {self.battery}%, Battery Saver Mode: {mode}"
+        return (
+            f"BnL Smartphone - Storage: {self.storage_capacity}GB, "
+            f"Battery: {self.battery}%, Battery Saver Mode: {mode}, "
+            f"Storage Left: {self.get_storage_left():.2f}GB"
+        )
     
 
 def test_smartphone():
@@ -35,6 +81,23 @@ def test_smartphone():
 
     phone.battery_saver_mode = True
     phone.charge_battery()
+    print(phone)
+
+def test_smartphone_with_apps():
+    phone = Smartphone(512)
+
+    phone.take_photo()
+    print(phone.photos_app)
+
+    phone.delete_photo()
+    print(phone.photos_app)
+
+    phone.create_post("My first post!")
+    print(phone.pictagram_app)
+
+    phone.delete_post()
+    print(phone.pictagram_app)
+
     print(phone)
 
 class PhotosApp:
@@ -114,8 +177,9 @@ def test_pictagram_app():
         print(app)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     test_smartphone()
+    test_smartphone_with_apps()
     test_photos_app()
     test_pictagram_app()
 
