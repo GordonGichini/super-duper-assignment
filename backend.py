54d8b436af5d2fa9d6830347d0f4d98034628c0c
GordonSoftbyte <gordon.gichini@softbyte.co.ke>
2026-04-14 09:24:17 +0300

    Pictagram class and test function

--- backend.py
+++ backend.py
@@ -75,8 +75,48 @@ def test_photos_app():
     print(app)
 
 
+class PictagramApp:
+    def __init__(self):
+        self.posts = []
+
+    def create_post(self, caption):
+        self.posts.append(caption)
+        print("Post created!")
+
+    def delete_post(self):
+        if len(self.posts) > 0:
+            self.posts.pop()
+            print("Post deleted!")
+
+    def calculate_storage_used(self):
+        total_mb = len(self.posts) * 15
+        total_gb = total_mb / 1024
+        return total_gb   
+    
+    def __str__(self):
+        storage = self.calculate_storage_used()
+        return f"Pictagram App - Posts: {len(self.posts)}, Storage Used: {storage:.2f}GB"
+    
+def test_pictagram_app():
+        app = PictagramApp()
+
+        # Create posts
+        app.create_post(" My Uno Post!")
+        app.create_post(" Having Fun with my Dos post!")
+        app.create_post(" Tres is the best post!")
+
+        # Print storage used
+        print("Storage used:", app.calculate_storage_used(), "GB")
+        print(app)
+
+        app.delete_post()
+
+        print(app)
+
+
 if __name__ == "__main__":
     test_smartphone()
     test_photos_app()
+    test_pictagram_app()
 
     
